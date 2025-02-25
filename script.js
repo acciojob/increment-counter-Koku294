08be94e9ad7da5f35eca7a0cfa9a927f7c544ff1
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-25 23:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,3 +15,10 @@
       // Update the displayed counter value
       counterElement.textContent = counter;
     });
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Returning false prevents Cypress from failing the test
+  if (err.message.includes('specific error message or part of the error')) {
+    return false;
+  }
+  // Otherwise, the test will fail on other uncaught exceptions
+});
